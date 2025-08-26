fffc5eedae02b84bd6e284a3a4ccf3a7d771db22
Devi <45352250+DeviMaya23@users.noreply.github.com>
2025-08-26 12:49:00 +1000

    feat: add life substracting feature in check answer

--- game.py
+++ game.py
@@ -1,6 +1,10 @@
 class Game:
     def __init__(self, word):
         self.word = word
+        self.life = 5
 
     def check_answer(self, answer):
-        return answer in self.word
+        result = answer in self.word
+        if not result:
+            self.life = self.life - 1
+        return result
--- game_test.py
+++ game_test.py
@@ -3,12 +3,13 @@ from game import Game
 
 
 @pytest.mark.parametrize(
-    ("word", "answer", "result"),
+    ("word", "answer", "result", "life"),
     [
-        ("Pity", "y", 1),
-        ("Pity", "a", 0),
+        ("Pity", "y", 1, 5),
+        ("Pity", "a", 0, 4),
     ]
 )
-def test_check_answer(word, answer, result):
-    logic = Game(word)
-    assert logic.check_answer(answer) == result
+def test_check_answer(word, answer, result, life):
+    game = Game(word)
+    assert game.check_answer(answer) == result
+    assert game.life == life
