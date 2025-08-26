80270e2549200a2e270ac8a751abce20577b482a
Devi <45352250+DeviMaya23@users.noreply.github.com>
2025-08-26 13:49:38 +1000

    feat: change list into set

--- game.py
+++ game.py
@@ -2,20 +2,20 @@ class Game:
     def __init__(self, word):
         self.word = word.lower()
         self.life = 5
-        self.revealed = []
-        self.correct_characters = []
+        self.revealed = set()
+        self.correct_characters = set()
 
     def check_answer(self, answer):
         found = 0
         answer = answer.lower()
         for i, character in enumerate(self.word):
             if character == answer:
-                self.revealed.append(i)
+                self.revealed.add(i)
                 found = 1
         if not found:
             self.life = self.life - 1
             return 0
-        self.correct_characters.append(answer)
+        self.correct_characters.add(answer)
         return 1
 
     def current_word(self):
--- game_test.py
+++ game_test.py
@@ -5,10 +5,10 @@ from game import Game
 @pytest.mark.parametrize(
     ("word", "answer", "result", "life", "revealed", "correct_characters"),
     [
-        ("pity", "y", 1, 5, [3], ["y"]),
-        ("pity", "a", 0, 4, [], []),
-        ("pity", "P", 1, 5, [0], ["p"]),
-        ("small", "l", 1, 5, [3, 4], ["l"])
+        ("pity", "y", 1, 5, {3}, {"y"}),
+        ("pity", "a", 0, 4, {}, {}),
+        ("pity", "P", 1, 5, {0}, {"p"}),
+        ("small", "l", 1, 5, {3, 4}, {"l"})
     ]
 )
 def test_check_answer(
@@ -23,9 +23,9 @@ def test_check_answer(
 @pytest.mark.parametrize(
     ("word", "revealed", "expected"),
     [
-        ("Pity", [3], "_ _ _ y"),
-        ("Pity", [], "_ _ _ _"),
-        ("Small", [0, 3, 4], "s _ _ l l")
+        ("Pity", {3}, "_ _ _ y"),
+        ("Pity", {}, "_ _ _ _"),
+        ("Small", {0, 3, 4}, "s _ _ l l")
     ]
 )
 def test_current_word(word, revealed, expected):
