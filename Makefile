f4161439c8ab34154fab899a83f7f6f1bb6eb99c
Devi <45352250+DeviMaya23@users.noreply.github.com>
2025-08-26 14:30:38 +1000

    feat: address pylint issues for game.py

---
+++ Makefile
@@ -0,0 +1,5 @@
+lint:
+	flake8 game.py
+	flake8 game_test.py
+	pylint game.py
+	pylint game_test.py
--- game.py
+++ game.py
@@ -1,27 +1,52 @@
+"""This module provides a class that represents a hangman game with its
+various functions."""
+
+
 class Game:
-    def __init__(self, word):
-        self.word = word.lower()
+    """This class represents a game of hangman."""
+
+    def __init__(self, answer):
+        """
+        Initialisation function for the class.
+        Args :
+        answer (string): Word or phrase used in the game.
+        """
+        self.answer = answer.lower()
         self.life = 5
         self.revealed = set()
         self.correct_characters = set()
 
-    def check_answer(self, answer):
+    def check_answer(self, guess):
+        """
+        Function that is run when the player submits an guess.
+        Checks whether the guess is correct and updates the game attributes
+        accordingly.
+
+        Args:
+        guess (string): A character submitted as a guess.
+        """
         found = 0
-        answer = answer.lower()
-        for i, character in enumerate(self.word):
-            if character == answer:
+        guess = guess.lower()
+        for i, character in enumerate(self.answer):
+            if character == guess:
                 self.revealed.add(i)
                 found = 1
         if not found:
             self.life = self.life - 1
             return 0
-        self.correct_characters.add(answer)
+        self.correct_characters.add(guess)
         return 1
 
-    def current_word(self):
+    def current_answer(self):
+        """Function that returns the current state of the answer
+
+        Returns:
+            string: The current word/phrase, with _ in the places of characters
+                that hasn't been guessed yet
+        """
         output = ""
         start_flag = 1
-        for i, character in enumerate(self.word):
+        for i, character in enumerate(self.answer):
             if start_flag:
                 start_flag = 0
             else:
@@ -33,4 +58,9 @@ class Game:
         return output
 
     def is_game_alive(self):
+        """Function that returns the current state of the game
+
+        Returns:
+            bool: Returns true if the game still has more than 0 life.
+        """
         return self.life > 0
