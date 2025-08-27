3d853b00e656d1f11084239fd8e4a3646d6bc033
Devi <45352250+DeviMaya23@users.noreply.github.com>
2025-08-27 10:47:45 +1000

    feat: add timer in console game

--- console_hangman.py
+++ console_hangman.py
@@ -2,9 +2,58 @@
 
 from game import Game
 from dictionary import Dictionary
+import time
+import threading
+from inputimeout import inputimeout, TimeoutOccurred
 
 
-INPUT_TIMER_SECOND = 15
+# Timer constant
+INPUT_TIMER_SECOND = 5
+
+
+# Global variable for threading purposes
+user_input = None
+is_countdown_finished = 0
+
+
+def countdown_timer():
+    """
+    Function used to print out a timer counting down.
+    """
+    global is_countdown_finished
+    for x in range(INPUT_TIMER_SECOND, 0, -1):
+        if is_countdown_finished:
+            break
+        print("Time:", x)
+        print("Enter your guess :")
+        time.sleep(1)
+
+
+def get_guess_timed():
+    """
+    Function used to get user's guess
+    with a timer counting down.
+    """
+    global is_countdown_finished
+    is_countdown_finished = 0
+
+    thread = threading.Thread(target=countdown_timer)
+    thread.daemon = True
+    thread.start()
+
+    guess = None
+    try:
+        guess = inputimeout(
+            prompt="", timeout=INPUT_TIMER_SECOND
+        )
+        is_countdown_finished = 1
+        thread.join()
+    except TimeoutOccurred:
+        is_countdown_finished = 1
+        thread.join()
+        guess = None
+
+    return guess
 
 
 def init_game(difficulty):
@@ -31,25 +80,32 @@ def run_game(game):
         Game: Game object
     """
     while game.is_game_alive():
+        print("===================================")
         print("Your clue is: ", game.current_answer())
         print("Attempts remaining: ", game.life)
+        print("===================================")
 
         while True:
-            guess = input("Enter your guess: ")
+            guess = get_guess_timed()
+            if guess is None:
+                game.deduct_life()
+                break
             try:
                 game.validate_guess(guess)
             except ValueError as err:
                 print("Input error: ", err)
             else:
                 break
+        if guess is None:
+            continue
         if game.check_answer(guess):
-            print("Correct guess!")
+            print("\nCorrect guess!")
             if game.is_game_finished():
-                print("Congratulations! You have won!")
+                print("\n\nCongratulations! You have won!")
                 break
         else:
-            print("Incorrect...")
+            print("\nIncorrect...")
             if not game.is_game_alive():
-                print("Game over :(")
+                print("\n\nGame over :(")
 
     print("The answer is: ", game.answer)
--- console_hangman_test.py
+++ console_hangman_test.py
@@ -1,6 +1,7 @@
 """Testing module"""
 from console_hangman import run_game
 from game import Game
+from inputimeout import TimeoutOccurred
 
 
 def test_run_game_win(monkeypatch):
@@ -9,15 +10,17 @@ def test_run_game_win(monkeypatch):
     when provided a winning input
     """
 
-    # Monkeypatch all console input()
-    inputs = iter(["easy", "a", "s", "h", "e", "n"])
-    monkeypatch.setattr("builtins.input", lambda _: next(inputs))
+    # Monkeypatch all console input
+    inputs = iter(["easy", "w", "h", "y"])
+    monkeypatch.setattr(
+        "console_hangman.inputimeout", lambda prompt, timeout: next(inputs))
+    monkeypatch.setattr("console_hangman.INPUT_TIMER_SECOND", 1)
 
     # The answer is fixed so the test can guess correctly
-    game = Game("ashen")
+    game = Game("why")
     run_game(game)
 
-    assert game.is_game_finished() == 1
+    assert game.is_game_finished() == 1  # checks if the game is won
 
 
 def test_run_game_losing(monkeypatch):
@@ -26,12 +29,45 @@ def test_run_game_losing(monkeypatch):
     when provided a winning input
     """
 
+    # Monkeypatch all console input
+    inputs = iter(["easy", "w", "h", "y", "s", "u"])
+    monkeypatch.setattr(
+        "console_hangman.inputimeout", lambda prompt, timeout: next(inputs))
+    monkeypatch.setattr("console_hangman.INPUT_TIMER_SECOND", 1)
+
+    game = Game("are")
+    run_game(game)
+
+    # assert that game is lost
+    assert game.is_game_finished() == 0  # checks if the game is won
+    assert game.is_game_alive() == 0  # checks if the game has 0 attempts
+
+
+def test_run_game_timeout(monkeypatch, capsys):
+    """
+    Test function to check the game state
+    when the timer to guess ran out
+    """
+
     # Monkeypatch all console input()
-    inputs = iter(["easy", "a", "s", "h", "e", "n"])
-    monkeypatch.setattr("builtins.input", lambda _: next(inputs))
+    def timeout(prompt, timeout):
+        raise TimeoutOccurred()
+
+    monkeypatch.setattr("console_hangman.INPUT_TIMER_SECOND", 2)
+    monkeypatch.setattr(
+        "console_hangman.inputimeout", timeout)
 
     game = Game("bully")
     run_game(game)
 
+    captured = capsys.readouterr()
+
+    # assert that the losing attempts are printed out
+    assert "Attempts remaining:  4" in captured.out
+    assert "Attempts remaining:  3" in captured.out
+    assert "Attempts remaining:  2" in captured.out
+    assert "Attempts remaining:  1" in captured.out
+
+    # assert that the game is lost
     assert game.is_game_finished() == 0
     assert game.is_game_alive() == 0
